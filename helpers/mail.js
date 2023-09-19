11733655e0b002c34452d3e22997047eb52fda18
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-09-19 17:12:44 +0530

    ADD: helper.mail => { sendEmailVerificationOtpEmail }

--- helpers/mail.js
+++ helpers/mail.js
@@ -19,4 +19,28 @@ mail.sendWelcomeMail = async (objMail) => {
     }
 }
 
+mail.sendEmailVerificationOtpEmail = async (objMail) => {
+    try {
+        return new Promise(async (resolve, reject) => {
+            let mailOptions = {
+                from: process.env.MAIL_FROM,
+                to: objMail.email,
+                subject: `OTP for email verification: ${objMail.username}`,
+                text: 'RentQube welcomes you',
+                html: `Hi, ${objMail.username}! <br>
+
+                Your registration is almost complete. do not share this otp with anyone.<br>
+                
+                If this is not you, please ignore this email.<br>
+                
+                The OTP code is: ${objMail.otp}`
+            }
+            let response = await mailer.sendEmail(mailOptions);
+            resolve(response);
+        })
+    } catch (error) {
+        reject(error);
+    }
+}
+
 module.exports = mail;
