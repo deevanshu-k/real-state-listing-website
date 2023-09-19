654a8c26339d9543535d63bff6f8405d832b6e08
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-09-19 17:15:56 +0530

    ADD: Route /api/register/verify For Email Verication By Otp

--- routes/register.js
+++ routes/register.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const tenant = require('../controllers/tenant.ctl');
 const landlord = require('../controllers/landlord.ctl');
+const otp = require('../controllers/otp.ctl');
 
 router.post('/tenant',tenant.register);
 router.post('/landlord',landlord.register);
+router.post('/verify',otp.verifyEmail);
 
 module.exports = router;
