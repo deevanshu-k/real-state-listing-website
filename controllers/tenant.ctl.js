6534996805f40839b7e572acf8ddbc330e99c83c
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-09-24 03:00:28 +0530

    UPDATE: tenantctl_formated

--- controllers/tenant.ctl.js
+++ controllers/tenant.ctl.js
@@ -17,13 +17,13 @@ tenant.register = async (req, res) => {
                 message: Constant.REQUEST_BAD_REQUEST
             });
         }
-        password = bcrypt.hashSync(password,salt);
+        password = bcrypt.hashSync(password, salt);
 
         // If User Already Exist With Not Verified Email
         let tenantData = await db.tenant.findOne({ where: { email: email, verified_email: false } });
         if (tenantData) {
             // Send Email For Otp
-            let otp = otpHelper.createOtpAndCacheOtp(email,8);
+            let otp = otpHelper.createOtpAndCacheOtp(email, 8);
             await mail.sendEmailVerificationOtpEmail({ email, username: tenantData.username, otp });
             // Redirect to email verification
             return res.status(Constant.SUCCESS_CODE).json({
@@ -60,15 +60,15 @@ tenant.register = async (req, res) => {
         // Check For Tenant Created
         if (tenantData) {
             // Send Email For Otp
-            let otp = otpHelper.createOtpAndCacheOtp(email,8);
+            let otp = otpHelper.createOtpAndCacheOtp(email, 8);
             await mail.sendEmailVerificationOtpEmail({ email, username: tenantData.username, otp });
             // Redirect to email verification
             return res.status(Constant.SUCCESS_CODE).json({
                 code: Constant.SUCCESS_CODE,
                 message: Constant.VERIFY_EMAIL,
                 data: {
-                    username : tenantData.username,
-                    email : tenantData.email,
+                    username: tenantData.username,
+                    email: tenantData.email,
                 }
             });
         }
@@ -80,13 +80,13 @@ tenant.register = async (req, res) => {
         }
     } catch (error) {
         // If Tenant Already Exist
-        if(error.name == 'SequelizeUniqueConstraintError'){
+        if (error.name == 'SequelizeUniqueConstraintError') {
             return res.status(Constant.BAD_REQUEST).json({
                 code: Constant.BAD_REQUEST,
                 message: Constant.EMAIL_ALREADY_REGISTERED
             });
         }
-        
+
         return res.status(Constant.SERVER_ERROR).json({
             code: Constant.SERVER_ERROR,
             message: Constant.REQUEST_SERVER_ERROR
