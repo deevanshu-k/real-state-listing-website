742e9c2244d6c1c03644316fe7c9ca1be68b69c2
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-09-24 03:39:12 +0530

    UPDATE: route/login RateLimiter ADDED

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const account = require("../controllers/account.ctl");
+const rateLimiter = require("../middileware/rate-limit");
 const router = express.Router();
 
-router.post('/tenant', (req, res, next) => { req.body.role = "TENANT"; next() }, account.login);
-router.post('/landlord', (req, res, next) => { req.body.role = "LANDLORD"; next() }, account.login);
-router.post('/admin', (req, res, next) => { req.body.role = "ADMIN"; next() }, account.login);
+router.post('/tenant', rateLimiter.loginRouteRateLimiter, (req, res, next) => { req.body.role = "TENANT"; next() }, account.login);
+router.post('/landlord', rateLimiter.loginRouteRateLimiter, (req, res, next) => { req.body.role = "LANDLORD"; next() }, account.login);
+router.post('/admin', rateLimiter.loginRouteRateLimiter, (req, res, next) => { req.body.role = "ADMIN"; next() }, account.login);
 
 module.exports = router;
