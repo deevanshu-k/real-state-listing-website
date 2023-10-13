64c39bec4bcd97c1fb6ea526b8e0062491ef1635
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-10-13 15:53:19 +0530

    ADD: rateLimiter To Property.Route + landlordOperationsRouteRateLimiter.middleware + paymentRouteRateLimiter.middleware + payment middleware updated

--- routes/payment.js
+++ routes/payment.js
@@ -4,7 +4,7 @@ const payment = require("../controllers/payment.ctl");
 const auth = require("../middileware/auth");
 const rateLimiter = require("../middileware/rate-limit");
 
-router.post("/order", rateLimiter.loginRouteRateLimiter, auth.checkAuthentication, payment.createOrder);
+router.post("/order", rateLimiter.paymentRouteRateLimiter, auth.checkAuthentication, payment.createOrder);
 router.post('/paymentCapture', payment.orderPaidWebhook);
 
 module.exports = router;
--- routes/property.js
+++ routes/property.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const property = require("../controllers/property.ctl");
 const auth = require("../middileware/auth");
 const checkUser = require("../middileware/user");
+const rateLimiter = require("../middileware/rate-limit");
 
 /*
     Landlord Info is set at req.user By auth.checkAuthentication Middleware
 */
-router.get("", auth.checkAuthentication, checkUser.landlord, property.getAllProperties);
+router.get("", rateLimiter.landlordOperationsRouteRateLimiter, auth.checkAuthentication, checkUser.landlord, property.getAllProperties);
 // Return all properties of landlord
-router.post("", auth.checkAuthentication, checkUser.landlord, property.createProperty);
+router.post("", rateLimiter.landlordOperationsRouteRateLimiter, auth.checkAuthentication, checkUser.landlord, property.createProperty);
 // Body Passed { property_name }
-router.put("", auth.checkAuthentication, checkUser.landlord, property.updateProperty);
+router.put("", rateLimiter.landlordOperationsRouteRateLimiter, auth.checkAuthentication, checkUser.landlord, property.updateProperty);
 // Body Passed { id , property_name }
-router.delete("", auth.checkAuthentication, checkUser.landlord, property.deleteProperty);
+router.delete("", rateLimiter.landlordOperationsRouteRateLimiter, auth.checkAuthentication, checkUser.landlord, property.deleteProperty);
 // Body Passed { id }
 // Delete Only If No Rooms Are present
 
