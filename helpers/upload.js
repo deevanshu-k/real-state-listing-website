621282fcb28a37208519fd41da7d6d8154fadb19
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-10-19 22:19:50 +0530

    ADD upload.helper.uploadDocument

--- helpers/upload.js
+++ helpers/upload.js
@@ -36,6 +36,31 @@ upload.uploadProfileImage = multer({
     }
 });
 
+upload.uploadDocument = multer({
+    limits: { fileSize: 5 * 1024 * 1024 },
+    storage: multerS3({
+        s3,
+        acl: 'public-read',
+        bucket: process.env.aws_bucket_name,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, cb) => {
+            const fileEmailHash = md5(req.user.email);
+            const type = req.params.type;
+            cb(null, `docs_${type}_${fileEmailHash}${path.extname(file.originalname)}`);
+        }
+    }),
+    fileFilter: function (req, file, cb) {
+        const fileRegex = new RegExp('\.(jpg|jpeg|png|pdf)$');
+        const fileMime = file.mimetype;
+        if (!fileMime.match(fileRegex)) {
+            //throw exception
+            return cb({ code: Constant.BAD_REQUEST, message: Constant.INVALID_DOCUMENT_TYPE, by: "multer" });
+        }
+        //pass the file
+        cb(null, true);
+    }
+});
+
 upload.multerErrorHandler = (err) => {
     // PENDING: Other Type Of Multer Error
     // https://github.com/expressjs/multer/blob/master/lib/multer-error.js
